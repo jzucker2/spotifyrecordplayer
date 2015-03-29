633cd3f8eb35ce2d7744a75ba20382c18354c61b
Jordan Zucker <jordan.zucker@gmail.com>
2015-03-29 01:04:13 -0700

    Simple podfile with pod and basic project

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+xcodeproj 'SpotifyRecordPlayer/SpotifyRecordPlayer.xcodeproj'
+
+platform :ios, '8.0'
+
+#pod 'AFNetworking', '~>2.5.2'
+pod 'BlocksKit', '~>2.2.5'
---
+++ SpotifyRecordPlayer/SpotifyRecordPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotifyRecordPlayer
+//
+//  Created by Jordan Zucker on 3/29/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
